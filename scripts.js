927716ba9daa08a2b5671357c8e4d7d0d3b2de91
Antoine Mahérault <antoine.maherault@laplateforme.io>
2021-10-21 14:45:08 +0200

    news

--- scripts.js
+++ scripts.js
@@ -11,32 +11,26 @@ function myFunction() {
   }
 }
 
+// SCROLL //
 
-// Slideshow //
+myID = document.getElementById("icon_arrow");
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
+var myScrollFunc = function () {
+  var y = window.scrollY;
+  if (y >= 800) {
+    document.getElementById("icon_arrow").style.display = "none";
+  } else {
+    document.getElementById("icon_arrow").style.display = "flex";
   }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+};
+
+window.addEventListener("scroll", myScrollFunc);
+
+
+// ________________ News ________________ //
+
+// Opacity //
+
+document.getElementById("three").style.opacity = "0.6";
+
+
